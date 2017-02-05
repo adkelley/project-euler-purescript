60a8f5578afe6c06b97361dbd2b648b1c9b6b5f3
Alex Kelley <adkelley@users.noreply.github.com>
2017-02-05 11:20:02 -0800

    Problem 5 naming

--- src/Problem5.purs
+++ src/Problem5.purs
@@ -2,23 +2,26 @@ module Problem5 (solution) where
 
 import Prelude
 
+-- Smallest Multiple
+-- see https://projecteuler.net/problem=5
+
 -- optimizations
 -- * num will always be divisible by 1 so start at 2
-evenlyDivisible :: Int -> Int -> Boolean
-evenlyDivisible rangeMax num = go 2 where
+isEvenlyDivisible :: Int -> Int -> Boolean
+isEvenlyDivisible rangeMax dividend = go 2 where
   go z
     | ((>) z rangeMax) = true
-    | not ((==) (mod num z) 0) = false
+    | not ((==) (mod dividend z) 0) = false
     | otherwise = go ((+) z 1)
 
 -- optimizations
 -- * A number must be even if it is divisible by all numbers
 --   so assuming maxRange is even, increment by 2 instead of 1
-smallestNumerator :: Int -> Int
-smallestNumerator rangeMax = go (rangeMax + 2) where
-  go z
-    | (evenlyDivisible rangeMax z) = z
-    | otherwise = go ((+) z 2)
+smallestMultiple :: Int -> Int
+smallestMultiple rangeMax = go ((+) rangeMax 2) where
+  go x = if (isEvenlyDivisible rangeMax x)
+         then x
+         else go ((+) x 2)
 
 solution :: Int -> Int
-solution rangeMax = smallestNumerator rangeMax
+solution rangeMax = smallestMultiple rangeMax
