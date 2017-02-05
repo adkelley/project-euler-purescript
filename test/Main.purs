42c5b18fdbc9a3421bbadd39f1d61f24a4ea0df0
Alex Kelley <adkelley@users.noreply.github.com>
2017-02-04 17:02:25 -0800

    Fixed test for Problem 3

--- test/Main.purs
+++ test/Main.purs
@@ -11,12 +11,6 @@ import Problem4 (solution4)
 test :: forall a. (Ord a) => (a -> a) -> a -> a -> Boolean
 test f x y = (f x) == y
 
-testNumber :: (Number -> Number) -> Number -> Number -> Boolean
-testNumber f x y = (f x) == y
-
--- testBigInt :: (BigInt -> BigInt) -> BigInt -> BigInt -> Boolean
--- testBigInt f x y = (f x) == y
-
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log $ "1: The sum of the multiples of 3 & 5 for natural numbers < 10 should equal 23 -> " <>
@@ -24,6 +18,6 @@ main = do
   log $ "2: The sum of the even-valued fibonacci terms < 89 should equal 44 -> " <>
   show (test solution2 89 44)
   log $ "3: The prime factors of 13195 are 5, 7, 13 and 29. The largest should be 29 -> " <>
-  show (testNumber solution3 13195.0 29.0)
+  show (test solution3 13195.0 29.0)
   log $ "4: The largest palindrome of the product of two 2-digits numbers should be 9009 -> " <>
   show (test solution4 10 9009)
